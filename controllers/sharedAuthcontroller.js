f702a6d6d052c995d6519dd94fe131566f2a1a61
AnietieDaniel <anietienteabasi@gmail.com>
2025-10-29 09:06:16 +0100

    In Christ Alone

--- controllers/sharedAuthcontroller.js
+++ controllers/sharedAuthcontroller.js
@@ -141,6 +141,7 @@ export const login = async (req, res) => {
       message: "Login successful",
       user: {
         id: user._id,
+        token: token,
         name: user.name,
         email: user.email,
         role: user.role,
